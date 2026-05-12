08e22d795896bfe68695ba0a550a7b2e6555a5e9
celestial4919 <celestial4919@gmail.com>
2026-05-12 11:53:04 +0300

    Reset Allowed Hosts

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['kiu-stores.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://kiu-stores.onrender.com']
 
 
